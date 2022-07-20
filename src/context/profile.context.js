59607079f52235aba1afb3b584f6ed2ee4e38d09
Rohan Saluja <rohansalujamusic@gmail.com>
2022-07-20 15:09:45 +0530

    displayed real-time presence

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -44,7 +44,7 @@ export const ProfileProvider = ({ children }) => {
         });
 
         database.ref(".info/connected").on("value", (snap) => {
-          if (snap.val() === false) {
+          if (!!snap.val() === false) {
             return;
           }
           userStatusRef
