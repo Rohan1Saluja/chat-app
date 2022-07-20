26f91d7cd23066430055138ab9248cbf92cac8df
Rohan Saluja <rohansalujamusic@gmail.com>
2022-07-20 17:55:52 +0530

    added button to grant or revoke admin access

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { memo } from "react";
+import { Button } from "rsuite";
 import TimeAgo from "timeago-react";
 import PresenceDot from "../../PresenceDot";
 import ProfileAvatar from "../../ProfileAvatar";
 import ProfileInfoBtnModal from "./ProfileInfoBtnModal";
+import { useCurrentRoom } from "../../../context/current-room.context";
+import { auth } from "../../../misc/firebase";
 
-const MessageItem = ({ message }) => {
+const MessageItem = ({ message, handleAdmin }) => {
   const { author, createdAt, text } = message;
+
+  const isAdmin = useCurrentRoom((v) => v.isAdmin);
+  const admins = useCurrentRoom((v) => v.admins);
+  const isMsgAuthorAdmin = admins.includes(author.uid);
+  const isAuthor = auth.currentUser.uid === author.uid;
+  const canGrantAdmin = isAdmin && !isAuthor;
+
   return (
     <li className="padded mb-1">
       <div className="d-flex align-items-center font-bolder mb-1">
@@ -16,12 +26,19 @@ const MessageItem = ({ message }) => {
           className="ml-1"
           size="xs"
         />
-        {/* <span className="ml-2">{author.name}</span> */}
         <ProfileInfoBtnModal
           profile={author}
           appearance="link"
           className="p-0 ml-1 text-black"
-        />
+        >
+          {canGrantAdmin && (
+            <Button block onClick={() => handleAdmin(author.uid)} color="blue">
+              {isMsgAuthorAdmin
+                ? "Remove admin permission"
+                : "Give admin in this room"}
+            </Button>
+          )}
+        </ProfileInfoBtnModal>
         <TimeAgo
           datetime={createdAt}
           className="ml-2 font-normal text-black-45"
@@ -35,4 +52,4 @@ const MessageItem = ({ message }) => {
   );
 };
 
-export default MessageItem;
+export default memo(MessageItem);
