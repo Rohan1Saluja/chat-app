2ddf398cd1ebad28e1397bc5aef16b91bd7137ef
Rohan Saluja <rohansalujamusic@gmail.com>
2022-07-23 14:10:29 +0530

    grouped messages by dates

--- src/misc/helpers.js
+++ src/misc/helpers.js
@@ -61,3 +61,24 @@ export async function getUserUpdates(userId, keyToUpdate, value, db) {
   return updates;
 }
 // !------------------------- Get User Updates -------------------------
+
+// ?----
+// ?----
+
+// !------------------------- Group By -------------------------
+
+export function groupBy(array, groupingKeyFn) {
+  return array.reduce((result, item) => {
+    const groupingKey = groupingKeyFn(item);
+
+    if (!result[groupingKey]) {
+      result[groupingKey] = [];
+    }
+
+    result[groupingKey].push(item);
+
+    return result;
+  }, {});
+}
+
+// !------------------------- Group By -------------------------
